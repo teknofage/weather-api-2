60193cc809519518cc3ffc967da8b23c87a7d1fc
teknofage <teknofage@gmail.com>
2020-06-06 14:11:56 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let request = require('request');
+
+let apiKey = '2608f679d4594364525f6c6cc2246c79';
+let city = 'portland';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+request(url, function (err, response, body) {
+  if(err){
+    console.log('error:', error);
+  } else {
+    let weather = JSON.parse(body)
+    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+    console.log(message);
+  }
+});
