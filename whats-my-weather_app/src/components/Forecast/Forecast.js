d7c354cabc30ec7046ece79cb3b19cbea2c33b6a
teknofage <teknofage@gmail.com>
2020-06-07 15:16:44 -0700

    committing before I get to error from last time

--- whats-my-weather_app/src/components/Forecast/Forecast.js
+++ whats-my-weather_app/src/components/Forecast/Forecast.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 const Forecast = () => {
-   function getForecast() {
-      // weather data fetch function will go here
-   }
+    let [responseObj, setResponseObj] = useState({});
+    function getForecast() {
+        fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=Seattle", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+            "x-rapidapi-key": "0bdb7e1f17msh8292f8f80e2b4dbp14f42bjsn0d32faa90f78"
+        }
+    })
+    .then(response => {
+        console.log(response);
+    })
+    .catch(err => {
+        console.log(err);
+    });   }
    return (
-       // JSX code will go here
-   )
+        <div>
+           <h2>Find Current Weather Conditions</h2>
+           <div>
+               {JSON.stringify(responseObj)}
+           </div>
+           <button onClick={getForecast}>Get Forecast</button>
+       </div>   
+       )
 }
 export default Forecast;
