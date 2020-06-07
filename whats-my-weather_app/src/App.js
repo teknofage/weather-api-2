e0dddb27e1ae54bc422225168e4d61038c7361f7
teknofage <teknofage@gmail.com>
2020-06-07 15:07:34 -0700

    adding API

--- whats-my-weather_app/src/App.js
+++ whats-my-weather_app/src/App.js
@@ -6,19 +6,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>What's My Weather?</h1>
       </header>
+      <main>
+        {/* add weather fetching component */}
+      </main>
+      <footer>
+        Page created by yournamehere
+      </footer>
     </div>
   );
 }
---
+++ whats-my-weather_app/src/components/Forecast/Forecast.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Forecast = () => {
+   function getForecast() {
+      // weather data fetch function will go here
+   }
+   return (
+       // JSX code will go here
+   )
+}
+export default Forecast;
